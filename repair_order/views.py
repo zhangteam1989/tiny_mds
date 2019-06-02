28ecaad9245077eef9af28bc55408d255e8deb9d
zhanghh <519241620@qq.com>
2019-06-02 22:10:53 +0800

    添加订单方法注释

--- repair_order/views.py
+++ repair_order/views.py
@@ -9,6 +9,9 @@ from django.shortcuts import render
 def create_order(request):
     """
     创建订单
+    1.订单号自动生成,订单号=RO+yyyyMMddHHmmss+5位数序列号
+    2.库存已经存在的imei不允许建单
+    3.同一个imei不允许存在非完结订单
     :param request:
     :return:
     """
@@ -18,6 +21,7 @@ def create_order(request):
 def submit_order(request):
     """
     提交订单
+    只有待提交状态的订单才能submit
     :param request:
     :return:
     """
@@ -27,6 +31,10 @@ def submit_order(request):
 def receipt(request):
     """
     收货
+    1.只有submit状态的订单才能收货
+    2.校验imei是否已经在库存中存在,存在则不允许收货
+    3.收货,坏件库位数量+1
+    4.修改收货状态为已收货
     :param request:
     :return:
     """
@@ -36,6 +44,7 @@ def receipt(request):
 def repair(request):
     """
     维修
+    1.只有已收货订单才能进行维修
     :param request:
     :return:
     """
